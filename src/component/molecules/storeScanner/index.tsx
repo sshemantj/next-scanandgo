b4077379d21ffc69921f91dbb056fe979557c542
Hemant Jadhav (Innova Consultant) <CON10201@shoppersstop.com>
2024-03-19 14:38:27 +0530

    changes

--- src/component/molecules/storeScanner/index.tsx
+++ src/component/molecules/storeScanner/index.tsx
@@ -102,7 +102,7 @@ const StoreScanner = () => {
   return (
     <div className={styles.storeScannerContainer}>
       <div className={styles.qrCodeScanWrapper}>
-        <h1>current device:= {currentDevice}</h1>
+        <h1 style={{ color: "#fff" }}>current device:= {currentDevice}</h1>
         {currentDevice === "iphone" && (
           <CustomHtml5Qrcode
             {...{ qrCodeSuccessCallback, qrCodeErrorCallback }}
